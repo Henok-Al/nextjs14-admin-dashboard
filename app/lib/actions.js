d27eccbdd93243badb3c5fde5dea9082bc2f633f
Henok Aemu <henok.gebresenbet@gmail.com>
2023-11-17 05:37:39 -0800

    update user and product using server action

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -36,6 +36,38 @@ export const addUser = async (FormData) => {
   redirect("/dashboard/users");
 };
 
+export const updateUser = async (formData) => {
+  const { id, username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const updateFields = {
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    };
+
+    Object.keys(updateFields).forEach(
+      (key) =>
+        (updateFields[key] === "" || undefined) && delete updateFields[key]
+    );
+
+    await User.findByIdAndUpdate(id, updateFields);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to update user!");
+  }
+
+  revalidatePath("/dashboard/users");
+  redirect("/dashboard/users");
+};
+
 export const addProduct = async (formData) => {
   const { title, desc, price, stock, color, size } =
     Object.fromEntries(formData);
@@ -62,6 +94,38 @@ export const addProduct = async (formData) => {
   redirect("/dashboard/products");
 };
 
+export const updateProduct = async (formData) => {
+  const { id, title, desc, price, stock, color, size } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+
+    const updateFields = {
+      title,
+      desc,
+      price,
+      stock,
+      color,
+      size,
+    };
+
+    Object.keys(updateFields).forEach(
+      (key) =>
+        (updateFields[key] === "" || undefined) && delete updateFields[key]
+    );
+
+    await Product.findByIdAndUpdate(id, updateFields);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to update product!");
+  }
+
+  revalidatePath("/dashboard/products");
+  redirect("/dashboard/products");
+};
+
+
 export const deleteProduct = async (formData) => {
   const { id } = Object.fromEntries(formData);
 
@@ -75,7 +139,6 @@ export const deleteProduct = async (formData) => {
 
   revalidatePath("/dashboard/products");
 };
-
 
 export const deleteUser = async (formData) => {
   const { id } = Object.fromEntries(formData);
